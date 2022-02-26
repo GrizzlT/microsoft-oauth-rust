c55cf648cd243b5ef37fa5852946acb29553692e
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2022-02-26 15:41:33 +0100

    Finalized api (for now)

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,31 @@
 #[macro_use]
 extern crate tracing;
 
+use isahc::HttpClient;
 use serde::Deserialize;
 
 pub use auth::{authenticate_or_refresh_microsoft, MicrosoftAuthData, TokenResponseError};
 pub use xbox::{authenticate_xbox_live, XblAuthData};
 pub use xsts::{authenticate_xsts, XstsAuthData};
+pub use error::MicrosoftAuthError;
 
-pub mod error;
+mod error;
 mod auth;
 mod xbox;
 mod xsts;
+
+/// Authenticates with microsoft and returns
+/// an XSTS-token.
+///
+/// * `old_data` will get updated
+/// * `redirect_uri` should be `http://localhost:<port>`
+pub async fn auth_microsoft_to_xsts(old_data: &mut Option<MicrosoftAuthData>, client_id: &str, redirect_uri: &str, port: u16) -> Result<XstsAuthData, MicrosoftAuthError> {
+    let client = HttpClient::new().unwrap();
+    authenticate_or_refresh_microsoft(old_data, client_id, redirect_uri, port, &client).await?;
+    let auth_data = old_data.as_ref().unwrap();
+    let xbox_data = authenticate_xbox_live(auth_data.access_token(), &client).await?;
+    authenticate_xsts(xbox_data.token(), &client).await
+}
 
 #[derive(Debug, Deserialize)]
 pub(crate) struct DisplayClaims {
